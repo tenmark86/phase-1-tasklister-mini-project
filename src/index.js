43dbeffa30a6bfd7ec36559d51c5c500477c9b5a
tenmark86e <tenmark86@gmail.com>
2022-08-23 11:48:17 +0300

    Done TaskLister Mini-Project assignmet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputField = document.getElementById("new-task-description");
+  const formElement = document.querySelector("form");
+  formElement.addEventListener('submit', (e) => {
+  let newtask =inputField.value
+
+    e.preventDefault();
+    if (newtask.length===0){
+    alert('Task cannot be empty')
+    }else{
+      handleToDo(newtask);
+    }
+    formElement.reset()
+  });
 });
+
+
+function css(element, style) {
+  for (const property in style)
+      element.style[property] = style[property];
+}
+
+function handleToDo (newtask) {
+  let listItem = document.createElement("li");
+  let btn = document.createElement('button')
+ 
+  css(btn, {
+    'background-color': 'yellow',
+    'color': 'red',
+     'background' :'red',
+  'color' :'#ffffff',
+  'padding' : '6px',
+'margin' : '4px',
+'font-size' :'12px',
+});
+
+  btn.addEventListener('click',handleDelete)
+  btn.textContent='X Remove'
+  listItem.innerText = `${newtask}`;
+  listItem.appendChild(btn)
+  document.querySelector("#tasks").appendChild(listItem);
+}
+
+function handleDelete(e){
+e.target.parentNode.remove()
+}
